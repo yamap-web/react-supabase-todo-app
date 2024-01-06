dcdfa4070f04fb5808cee5c689bd8e695086e69d
YAMAGUCHI Tomohisa <intelligence.forever.2003@gmail.com>
2024-01-06 12:19:24 +0900

    Update Supabase URL in supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  'SUPABASE_URL',
+  'REACT_APP_SUPABASE_URL',
   'REACT_APP_SUPABASE_ANON_KEY'
 );
