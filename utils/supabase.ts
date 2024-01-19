150eab99d55697f50dac8d54a383eb82ad610b66
YAMAGUCHI Tomohisa <intelligence.forever.2003@gmail.com>
2024-01-19 22:58:28 +0900

    Update Supabase environment variables

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
+export const supabase = createClient(process.env.PUBLIC_SUPABASE_URL!, process.env.PUBLIC_SUPABASE_ANON_KEY!);
