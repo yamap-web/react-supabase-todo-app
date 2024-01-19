4df5af49c4aa5150b218d44e57e8e3ff6ccb21b4
YAMAGUCHI Tomohisa <intelligence.forever.2003@gmail.com>
2024-01-19 23:06:48 +0900

    Update Supabase URL and Anon Key

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://bejcujkzypljtonizcgz.supabase.co'!;
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJlamN1amt6eXBsanRvbml6Y2d6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0NTYzMjEsImV4cCI6MjAyMDAzMjMyMX0.rEc2LlsCJYdeDaeK_4ikippMcnJBCK3EZEmdEnKzxrc'!;
+
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(process.env.REACT_PUBLIC_SUPABASE_URL!, process.env.REACT_PUBLIC_SUPABASE_ANON_KEY!);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
