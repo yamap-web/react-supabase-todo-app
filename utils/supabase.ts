f4c471349d528dabfebe702e28ed7a875a06792a
YAMAGUCHI Tomohisa <intelligence.forever.2003@gmail.com>
2024-01-19 22:50:26 +0900

    Refactor supabase client initialization

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey =
-  process.env.SUPABASE_ANON_KEY;
-
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
+export const supabase = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_ANON_KEY!);
