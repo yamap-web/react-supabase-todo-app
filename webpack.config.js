6f72f835c7508d9033b134b410def01e08e6b70d
YAMAGUCHI Tomohisa <intelligence.forever.2003@gmail.com>
2024-01-20 16:39:40 +0900

    Add copy-webpack-plugin for copying files to dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 const workBoxWebpackPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -51,12 +52,21 @@ module.exports = {
     new workBoxWebpackPlugin.GenerateSW({
       swDest: __dirname + '/dist' + '/service-worker.js',
     }),
-    new WebpackPwaManifest({
-      short_name: 'short name', // ホーム画面のラベルに表示される名称
-      name: 'app full name', // appの名前
-      display: 'standalone', // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
-      start_url: 'index.html', // 開始時に起動するページ
+    // new WebpackPwaManifest({
+    //   short_name: 'short name', // ホーム画面のラベルに表示される名称
+    //   name: 'app full name', // appの名前
+    //   display: 'standalone', // standaloneにするとブラウザのUI要素が削除されてアプリっぽくなる
+    //   start_url: 'index.html', // 開始時に起動するページ
+    // }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '.',
+        },
+      ],
     }),
+    // webpackでもpublicフォルダの中身をdistにコピーする必要がある
   ],
   devServer: {
     static: {
