5dc44bfcdef36bb12bc3e7303fdc972fb186b520
yamap <yamap.web@gmail.com>
2024-01-21 18:35:56 +0900

    Add webpack.DefinePlugin for environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const webpack = require('webpack');
 const workBoxWebpackPlugin = require('workbox-webpack-plugin');
 // const WebpackPwaManifest = require('webpack-pwa-manifest');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
@@ -49,6 +50,9 @@ module.exports = {
       template: __dirname + '/src/index.html',
     }),
     new Dotenv(),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(env)
+    }),
     new workBoxWebpackPlugin.GenerateSW({
       swDest: __dirname + '/dist' + '/service-worker.js',
     }),
